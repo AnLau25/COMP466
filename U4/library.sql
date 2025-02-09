5018cfb449ac97f6f9dded537be1e1a5ffc87599
AnLau25 <avald024@uottawa.ca>
2025-02-09 13:15:16 -0500

    forgot to quit

--- U4/library.sql
+++ U4/library.sql
@@ -15,5 +15,13 @@ CREATE TABLE borrowers
    Borrower_ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
    FirstName varchar(60),
    LastName varchar(32),
-   BirthDate date(20)
+   BirthDate date
 );
+
+-- So, script is run, and you can source, SQL will create and edit and whatnot, once that is done, it WILL be saved
+-- Ie, don't bother runnig more than once
+/*
+mysql> select * from books
+    -> \G
+*/
+--Rhum666cric 
