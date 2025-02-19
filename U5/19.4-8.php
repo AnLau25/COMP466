77fb8ff3c5b45b2edfae14af9be20d4933066760
AnLau25 <avald024@uottawa.ca>
2025-02-19 11:52:38 -0500

    phpisphping

--- U5/19.4-8.php
+++ U5/19.4-8.php
@@ -6,18 +6,20 @@
     <title>Finding patterns</title>
 </head>
     <?php
-        $ptrn = "/^A[a-zA-Z0-9]{3}[Bb]+\\d{2}$/";
+        $ptrnS = "/^A[a-zA-Z0-9]{3}[Bb]+\\d{2}$/";
         //^ must be at start 
         //A means that it has to be that A
         //[a-zA-Z0-9]{3} alphanum and count 3; could've ussed \w, but that one includes _
         //[Bb]+ one or more occurences
         // \\d{2} 2 digits, \\ cause more than one, so \d for one and \\d+
         // Assert the string
+        $ptrnE = "/^.+[@].+[.].+$/";
         $strA = "Alo5b14"; 
         $strB = "Vet5b14";
         $strC = "Alo+nonalpha";
         $strD = "Alo5Cnotb";
         $strE = "Alo5bnonums";
+        $mailA= "ana@gmail.com";
     ?>
 <body>
     <?php
@@ -29,11 +31,12 @@
             };
         };
 
-        stringCompare($ptrn, $strA);
-        stringCompare($ptrn, $strB);
-        stringCompare($ptrn, $strC);
-        stringCompare($ptrn, $strD);
-        stringCompare($ptrn, $strE);
+        stringCompare($ptrnS, $strA);
+        stringCompare($ptrnS, $strB);
+        stringCompare($ptrnS, $strC);
+        stringCompare($ptrnS, $strD);
+        stringCompare($ptrnS, $strE);
+        stringCompare($ptrnE, $mailA);
 
     ?>  
 </body>
