979041319d818f3f4d1fdd58f54c22691f8f4db4
AnLau25 <avald024@uottawa.ca>
2025-02-19 10:38:02 -0500

    save

--- U5/19.4-8.php
+++ U5/19.4-8.php
@@ -5,7 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Finding patterns</title>
 </head>
+    <?php
+        $ptrn = "/^A[a-zA-Z0-9]{3}[Bb]+\\d{2}$/";
+        //^ must be at start 
+        //A means that it has to be that A
+        //[a-zA-Z0-9]{3} alphanum and count 3; could've ussed \w, but that one includes _
+        //[Bb]+ one or more occurences
+        // \\d{2} 2 digits, \\ cause more than one, so \d for one and \\d+
+        // Assert the string
+        $strA = "Alo5b14"; 
+        $strB = "Vet5b14";
+        $strC = "Alo+nonalpha";
+        $strD = "Alo5Cnotb";
+        $strE = "Alo5bnonums";
+    ?>
 <body>
-    
+    <?php
+        function stringCompare($p, $s){
+            if(preg_match($p, $s)){
+                echo "Eso tilin! <br>";
+            }else{
+                echo "No Mussolini no... <br>";
+            };
+        };
+
+        stringCompare($ptrn, $strA);
+        stringCompare($ptrn, $strB);
+        stringCompare($ptrn, $strC);
+        stringCompare($ptrn, $strD);
+        stringCompare($ptrn, $strE);
+
+    ?>  
 </body>
 </html>
