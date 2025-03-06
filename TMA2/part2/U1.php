2db7359f37be9a5cfd0c0b85ce090a5caed9d6ee
AnLau25 <avald024@uottawa.ca>
2025-03-06 14:01:54 -0500

    FIt,we'reReadingFromFile

---
+++ TMA2/part2/U1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unit 1: HTML5 &amp; CSS</title>
+</head>
+<body>
+    
+</body>
+</html>
