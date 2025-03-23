73a712c33cfe97bbbbf4b53bc4d161ad999e85fd
AnLau25 <avald024@uottawa.ca>
2025-03-23 10:09:39 -0400

    updateBeforeCleaning

---
+++ U6/Phonetest2/Phonetest2/Models/ContactDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonetest2.Models
+{
+	public class ContactDto
+	{
+		[Required(ErrorMessage = "Nombre necesario")]
+		public string Firstname { get; set; } = "";
+		[Required(ErrorMessage = "Apellido necesario")]
+		public string Lastname { get; set; } = "";
+		[Required(ErrorMessage = "Numero necesario")]
+		public string Ponenum { get; set; } = "";
+
+	}
+}
