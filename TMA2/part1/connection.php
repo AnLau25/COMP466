42189352089027fcc252baba81f272ab60ed1fcc
AnLau25 <avald024@uottawa.ca>
2025-04-03 22:37:22 -0400

    indeed

--- TMA2/part1/connection.php
+++ TMA2/part1/connection.php
@@ -1,11 +1,10 @@
 <?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "bookmarks";
-    $port = 33;
+    $hostname = "sql204.infinityfree.com";
+    $username = "if0_38668808";
+    $password = "TPtwDTdJALbwB";
+    $dbname = "if0_38668808_bookmarks";
     
-    $database = mysqli_connect( $hostname, $username, $password, $dbname, $port);
+    $database = mysqli_connect( $hostname, $username, $password, $dbname);
     if ($database->connect_error) {
         die("Error en la conexión: ".$database->connect_error);
     }
