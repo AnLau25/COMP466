36fe892c8d4d5d23c43edd593d075abd8da7f6ac
AnLau25 <avald024@uottawa.ca>
2025-04-21 22:50:03 -0400

    ToldUIHadStuffToCommit

---
+++ U6/20.7/SessionClick.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SessionClick.aspx.cs" Inherits="MailBook.SessionClick" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Your visits will apear shortly</h1>
+    <form id="form1" runat="server">
+        <p style="display:flex;flex-direction:column;">
+            <asp:Label runat="server" ID="Sess"/>
+            <asp:Label runat="server" ID="IP"/>
+            <asp:Label runat="server" ID="TimeZone"/>
+        </p>
+    </form>
+</body>
+</html>
---
+++ U6/20.7/SessionClick.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using static System.Collections.Specialized.BitVector32;
+
+namespace MailBook
+{
+	public partial class SessionClick : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (Session["visits"] == null)
+			{
+				Session["visits"] = "1";
+				Session.Timeout = 1140;
+			}else
+			{
+				Session["visits"] = (int.Parse((Session["visits"]).ToString()) + 1).ToString();
+			}
+
+			Sess.Text = "You've been here " + (Session["visits"]).ToString() + " times";
+
+			IP.Text = "Your location is " + Request.UserHostAddress.ToString();
+
+			TimeZone.Text = "Your time zone is " + TimeZoneInfo.Local.StandardName.ToString();
+
+
+
+		}
+			
+	}
+}
